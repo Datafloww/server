36111b4417d9adbfb685af658ea277adfe6cc2a7
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-02-22 17:29:11 +0530

    add:schema

---
+++ src/db/schema/clients.ts
@@ -0,0 +1,21 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const clients = pgTable("client", {
+    id: serial("id"),
+    clientId: text("client_id").notNull().unique().primaryKey(),
+    email: text("email").notNull().unique(),
+});
+
+export const apiKeys = pgTable("api_keys", {
+    id: text("id")
+        .primaryKey()
+        .references(() => clients.clientId, {
+            onDelete: "cascade",
+        }),
+    keyHash: text("key_hash").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at", {
+        mode: "date",
+        precision: 3,
+    }).$onUpdate(() => new Date()),
+});
