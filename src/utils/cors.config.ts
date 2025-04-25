2c5c35d4b50d243792c3dfcea151de51c2e23076
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-04-25 20:55:28 +0530

    update cors headers

--- src/utils/cors.config.ts
+++ src/utils/cors.config.ts
@@ -4,12 +4,6 @@ const cors_config: CorsOptions = {
     origin: ["https://dash.datafloww.me", "http://dash.datafloww.me"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "X-Client-Version",
-        "X-Analytics-Source",
-    ],
 };
 
 export default cors_config;
