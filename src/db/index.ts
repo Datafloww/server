a53a86f32fb22ddddab2212828b1096b698d7e9f
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-04-26 13:14:04 +0530

    fix:db conf

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,16 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+export const db = drizzle(pool, {
+    logger: true,
+});
 
 export { clients, apiKeys } from "./schema/clients.js";
 export { events, sessions } from "./schema/analytics.js";
-
-export const db = drizzle(process.env.DATABASE_URL!);
