efab5906cba2fffd5e17125c0252d1e81b55012c
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-04-26 14:03:02 +0530

    fix:db conf

--- src/utils/cors.config.ts
+++ src/utils/cors.config.ts
@@ -1,18 +1,9 @@
 import { CorsOptions } from "cors";
 
-const allowedOrigins = [
-    "https://dash.datafloww.me",
-    "http://dash.datafloww.me",
-];
+// const allowedOrigins = ["*"];
 
 const cors_config: CorsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
